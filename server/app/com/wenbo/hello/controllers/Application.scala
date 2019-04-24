e4f7ee904275fb55b6420525019c0fb98c28d819
gaowenbo <gaowenbo@mankebao.com>
2019-04-24 17:32:17 +0800

    fix try websocket

--- server/app/com/wenbo/hello/controllers/Application.scala
+++ server/app/com/wenbo/hello/controllers/Application.scala
@@ -14,12 +14,11 @@ import play.api.mvc._
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
 
-
-
 @Singleton
 class Application @Inject()(@Named("userParentActor") userParentActor: ActorRef,
-                            cc: ControllerComponents)(implicit ec: ExecutionContext)
-                            extends AbstractController(cc) {
+cc: ControllerComponents)
+(implicit ec: ExecutionContext)
+extends AbstractController(cc) with SameOriginCheck {
 
   val logger = play.api.Logger(getClass)
 
@@ -37,7 +36,7 @@ class Application @Inject()(@Named("userParentActor") userParentActor: ActorRef,
       "</html>")
   }
   def ws: WebSocket = WebSocket.acceptOrResult[JsValue, JsValue] {
-    case rh =>
+    case rh if sameOriginCheck(rh) =>
       wsFutureFlow(rh).map { flow =>
         Right(flow)
       }.recover {
@@ -67,41 +66,41 @@ class Application @Inject()(@Named("userParentActor") userParentActor: ActorRef,
   }
 
 }
-//trait SameOriginCheck {
-//
-//  def logger: Logger
-//
-//  /**
-//    * Checks that the WebSocket comes from the same origin.  This is necessary to protect
-//    * against Cross-Site WebSocket Hijacking as WebSocket does not implement Same Origin Policy.
-//    *
-//    * See https://tools.ietf.org/html/rfc6455#section-1.3 and
-//    * http://blog.dewhurstsecurity.com/2013/08/30/security-testing-html5-websockets.html
-//    */
-//  def sameOriginCheck(rh: RequestHeader): Boolean = {
-//    rh.headers.get("Origin") match {
-//      case Some(originValue) if originMatches(originValue) =>
-//        logger.debug(s"originCheck: originValue = $originValue")
-//        true
-//
-//      case Some(badOrigin) =>
-//        logger.error(s"originCheck: rejecting request because Origin header value ${badOrigin} is not in the same origin")
-//        false
-//
-//      case None =>
-//        logger.error("originCheck: rejecting request because no Origin header found")
-//        false
-//    }
-//  }
-//
-//  /**
-//    * Returns true if the value of the Origin header contains an acceptable value.
-//    *
-//    * This is probably better done through configuration same as the allowedhosts filter.
-//    */
-//  def originMatches(origin: String): Boolean = {
-////    origin.contains("localhost:9000") || origin.contains("localhost:19001")
-//    true
-//  }
-//
-//}
+trait SameOriginCheck {
+
+  def logger: Logger
+
+  /**
+    * Checks that the WebSocket comes from the same origin.  This is necessary to protect
+    * against Cross-Site WebSocket Hijacking as WebSocket does not implement Same Origin Policy.
+    *
+    * See https://tools.ietf.org/html/rfc6455#section-1.3 and
+    * http://blog.dewhurstsecurity.com/2013/08/30/security-testing-html5-websockets.html
+    */
+  def sameOriginCheck(rh: RequestHeader): Boolean = {
+    rh.headers.get("Origin") match {
+      case Some(originValue) if originMatches(originValue) =>
+        logger.debug(s"originCheck: originValue = $originValue")
+        true
+
+      case Some(badOrigin) =>
+        logger.error(s"originCheck: rejecting request because Origin header value ${badOrigin} is not in the same origin")
+        false
+
+      case None =>
+        logger.error("originCheck: rejecting request because no Origin header found")
+        false
+    }
+  }
+
+  /**
+    * Returns true if the value of the Origin header contains an acceptable value.
+    *
+    * This is probably better done through configuration same as the allowedhosts filter.
+    */
+  def originMatches(origin: String): Boolean = {
+//    origin.contains("localhost:9000") || origin.contains("localhost:19001")
+    true
+  }
+
+}
